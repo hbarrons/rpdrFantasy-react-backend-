8d2faca67517a0acb9a0fefa7f950582ce37000b
hbarrons <hbarrons@gmail.com>
2022-04-18 11:48:09 -0400

    stub up Profile model

--- models/profile.js
+++ models/profile.js
@@ -1,12 +1,54 @@
 import mongoose from 'mongoose'
 
+const rosterSchema = new mongoose.Schema({
+  queen1: {
+    type: String,
+  },
+  queen2: {
+    type: String,
+  },
+  queen3: {
+    type: String,
+  },
+  queen4: {
+    type: String,
+  },
+})
+
+const guessSchema = new mongoose.Schema({
+  queen1: {
+    type: String,
+  },
+  queen2: {
+    type: String,
+  },
+  episode: {
+    type: Number,
+    min: 1,
+    max: 17,
+  },
+}, {
+  timestamps: true
+})
+
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
-},{
-    timestamps: true,
+  avatar: String,
+  guessSeason: {
+    type: String,
+  },
+  favQueen: {
+    type: String,
+  },
+  roster: [rosterSchema],
+  guessEpisode: [guessSchema],
+}, {
+  timestamps: true
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
 
-export {Profile}
+export {
+  Profile
+}
