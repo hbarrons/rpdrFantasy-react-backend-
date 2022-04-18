e32d37bf5d854455600e5126bc1d91b62eb0044d
hbarrons <hbarrons@gmail.com>
2022-04-18 11:52:40 -0400

    stub up episode model

---
+++ models/episode.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const episodeSchema = new mongoose.Schema({
+  number: {
+    type: Number,
+    unique: true
+  },
+  winner: {
+    type: String,
+  },
+  loser: {
+    type: String,
+  },
+  tops: {
+    type: []
+  },
+  bottoms: {
+    type: []
+  }
+}, {
+  timestamps: true
+})
+
+const Episode = mongoose.model('Episode', episodeSchema)
+
+export {
+  Episode
+}
