5c85ebaca09d960ceb0026e1f4b87318607528d4
hbarrons <hbarrons@gmail.com>
2022-04-20 15:34:35 -0400

    add league info to database

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -11,13 +11,17 @@ export function index(req, res) {
 
 export function createLeague(req,res) {
   console.log("body: ", req.body)
-  console.log("req.params: ", req.params)
-  // Profile.findById(req.params.userId)
-  // .then(profile => console.log("profile: ", profile))
-  // .catch(err => {
-  //   console.log(err)
-  //   res.status(500).json(err)
-  // })
+  console.log("req.params.userId: ", req.params.userId)
+  Profile.findById(req.params.userId)
+  .then(profile => {
+    profile.league.push({name: req.body.leagueName, leagueNo: req.body.leagueNo})
+    profile.save()
+    res.status(201).json(profile)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 }
 
 
